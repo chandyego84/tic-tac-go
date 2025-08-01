cadaec0ae64b53f0926604da78908653baf36a2e
chandyego <chandler.juego@wsu.edu>
2025-08-01 16:26:22 -0700

    chat app starting point from websocket

---
+++ THIRD_PARTY.md
@@ -0,0 +1,7 @@
+## Third-Party Code
+
+This project uses code from the Gorilla WebSocket library, which is licensed under the BSD license.
+
+- Copyright 2013 The Gorilla WebSocket Authors
+- Source: https://github.com/gorilla/websocket
+- License: BSD-style license
--- main.go
+++ main.go
@@ -1,50 +1,40 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
+
 import (
-	"fmt"
+	"flag"
+	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
 )
 
-/*
-* Upgrades an HTTP connection to a websocket
-*/
-var upgrader = websocket.Upgrader {
-	// allow all connections (in prod, should validate origin to avoid cross-site websocket hijacking)
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
+var addr = flag.String("addr", ":8080", "http service address")
 
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// upgrade GET request to websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if (err != nil) {
-		fmt.Println(err.Error())
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
 		return
 	}
-	defer ws.Close() // stupid language
-
-	for { // read/write loop -- read messages from client and echo them back
-		// read msgs from browser
-		_, msg, err := ws.ReadMessage()
-		if (err != nil) {
-			fmt.Println("read error: ", err.Error())
-			break
-		}
-		fmt.Printf("Received: %s\n", msg)
-
-		// write msg back to browser
-        if err := ws.WriteMessage(websocket.TextMessage, msg); err != nil { 
-            fmt.Println("write error:", err)
-            break
-        }
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
+	http.ServeFile(w, r, "home.html")
 }
 
 func main() {
-	http.HandleFunc("/ws", handleConnections)
-
-	fmt.Println("Websocket server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if (err != nil) {
-		fmt.Println("ListenAndServe: ", err.Error())
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
