848a92db2ab66f06a762442ef496bf3205b1deae
Chandler Juego <chandlerjuego101@gmail.com>
2025-08-05 15:45:55 -0700

    adding more comments for my noob self

--- client.go
+++ client.go
@@ -7,10 +7,10 @@ package main
 import (
 	"bytes"
 	"log"
-	"net/http"
-	"time"
 	"math/rand"
-	"strconv" 
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -49,17 +49,18 @@ type Client struct {
 	// name of peer
 	clientName string
 
-	// Buffered channel of outbound messages.
+	// Buffered channel of outbound messages to clients.
 	send chan []byte
 }
 
+// Message wraps the user and the message data.
 type Message struct {
 	user string
 
 	message string
 }
 
-// readPump pumps messages from the websocket connection to the hub.
+// readPump reads messages from the client and sends them to the hub
 //
 // The application runs readPump in a per-connection goroutine. The application
 // ensures that there is at most one reader on a connection by executing all
@@ -86,45 +87,66 @@ func (c *Client) readPump() {
 	}
 }
 
-// writePump pumps messages from the hub to the websocket connection.
+// writePump writes messages from the hub to the client (sends outbound messages)
 //
 // A goroutine running writePump is started for each connection. The
 // application ensures that there is at most one writer to a connection by
 // executing all writes from this goroutine.
 func (c *Client) writePump() {
-	ticker := time.NewTicker(pingPeriod)
+	ticker := time.NewTicker(pingPeriod) // sends periodic ping messages to keep the connection alive
+
 	defer func() {
+		// Stop the ticker and close the WebSocket connection when this function exits
 		ticker.Stop()
 		c.conn.Close()
 	}()
+
+	// Loop forever, waiting to either:
+	// - receive a message to send from c.send
+	// - or send a periodic ping to keep the connection alive
 	for {
 		select {
+
+		// Case 1: message ready to be sent to client
 		case message, ok := <-c.send:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+
 			if !ok {
-				// The hub closed the channel.
+				// The `c.send` channel was closed (e.g., hub unregistered the client)
+				// Send a close message to the WebSocket client
 				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
 
+			// Start writing a new WebSocket message of type "TextMessage"
+			// This returns a writer that allows streaming large messages
 			w, err := c.conn.NextWriter(websocket.TextMessage)
 			if err != nil {
+				// If we can't get a writer (maybe connection is closed), stop
 				return
 			}
+
+			// Write the current message into the writer
 			w.Write(message)
 
-			// Add queued chat messages to the current websocket message.
+			// Also write any **queued messages** that came in while we were writing
+			// This helps batch multiple messages into a single WebSocket frame
 			n := len(c.send)
 			for i := 0; i < n; i++ {
 				w.Write(newline)
 				w.Write(<-c.send)
 			}
 
+			// Finish writing and flush everything to the network
 			if err := w.Close(); err != nil {
 				return
 			}
+
+		// Case 2: time to send a ping message to keep the connection alive
 		case <-ticker.C:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+
+			// Send a Ping control message (used to detect dropped connections)
 			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
 				return
 			}
@@ -132,9 +154,11 @@ func (c *Client) writePump() {
 	}
 }
 
+// Creates a new message with user and message content.
+// This is a utility function to encapsulate message creation.
 func newMessage(user string, message string) *Message {
 	return &Message{
-		user: user,
+		user:    user,
 		message: message,
 	}
 }
